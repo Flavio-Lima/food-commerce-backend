26623dbad6de0ba9ecd09826ee6bef56649e93c9
Flavio Lima <flavio.lima981@gmail.com>
2023-09-24 09:38:17 -0300

    cria endpoint snacks

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import dotenv from 'dotenv';
-import express, {Express, Request, Response} from 'express';
+import { PrismaClient } from "@prisma/client"
+import dotenv from "dotenv"
+import express, { Express, Request, Response } from "express"
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = process.env.PORT || 5000;
+const app: Express = express()
+const port = process.env.PORT || 5000
+const prisma = new PrismaClient()
 
 app.use(express.json())
 
@@ -14,6 +16,23 @@ app.get("/", (req: Request, res: Response) => {
   if (!message) return res.status(400).send({ error: "Message is required" })
 
   res.send({ message })
+})
+
+app.get("/snacks", async (req: Request, res: Response) => {
+  const { snack } = req.query
+
+  if (!snack) return res.status(400).send({ error: "Snack is required" })
+
+  // SELECT * FROM Snack WHERE snack = 'drink'
+  const snacks = await prisma.snack.findMany({
+    where: {
+      snack: {
+        equals: snack as string,
+      },
+    },
+  })
+
+  res.send(snacks)
 })
 
 app.listen(port, () => {
